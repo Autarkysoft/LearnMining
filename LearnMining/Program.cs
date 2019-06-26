6b460da9db4d567544dd2fa822a834088a28d908
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2019-06-26 17:58:19 +0430

    Added ScryptMiner so that options make sense!

--- LearnMining/Program.cs
+++ LearnMining/Program.cs
@@ -6,25 +6,34 @@ namespace LearnMining
     {
         static void Main(string[] args)
         {
-            int option = Helper.ReadInt(1, 1);
+            Console.WriteLine("Choose which miner to run, options are:");
+            Console.WriteLine("1. Bitcoin miner (double SHA-256).");
+            Console.WriteLine("2. Litecoin miner (scrypt).");
+
+            int option = Helper.ReadInt(1, 2);
+
             IMiner miner = null;
             switch (option)
             {
                 case 1:
                     miner = new DoubleSha256Miner();
                     break;
+                case 2:
+                    miner = new ScryptMiner();
+                    break;
             }
 
             if (miner != null)
             {
                 miner.Mine();
-                Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("Could not initialize miner...");
                 Console.WriteLine("Exiting...");
             }
+
+            Console.ReadLine();
         }
 
     }
---
+++ LearnMining/ScryptMiner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace LearnMining
+{
+    #region Explanation
+
+    /*
+     * Mining litecoin is is exactly similar to mining bitcoin except it uses scrypt KDF as its hash algorithm
+     * 
+     * ***** The data *****
+     * Data is the same as bitcoin's
+     * 
+     * ***** scrypt *****
+     * Hash function used for litecoin is scrypt which is an expensive key derivation function.
+     * Scrypt at heart uses PBKDF2 (RFC-8018) and HMAC-SHA-256 while it has its own internal block mixing algorithm.
+     * 
+     * 
+     * ***** The loop *****
+     * 
+     * 
+     * 
+     * */
+
+    #endregion
+
+    public class ScryptMiner : IMiner
+    {
+        public unsafe void Mine()
+        {
+            // from https://litecoin.info/index.php/Scrypt
+            uint blockVersion = 0x01000000U;
+            // Since litecoin is a copy of bitcoin it uses the same hash algorithm as bitcoin to report block "hashes"!
+            // and does it in the same reverse order
+            byte[] prvBlockHash = Helper.HexToBytes("05e9a54b7f65b46864bc90f55d67cccd8b6404a02f5e064a6df69282adf6e2e5 ", true);
+            byte[] merkle = Helper.HexToBytes("f7f953b0632b25b099858b717bb7b24084148cfa841a89f106bc6b655b18d2ed ", true);
+            uint blockTime = 0x1a19bb4eU;
+            uint nBits = 0xa78e011dU;
+            uint startingNonce = 0x42a14695U - 50_000_000U;
+
+
+            Console.WriteLine("Coming soon!");
+
+
+
+        }
+
+    }
+}
